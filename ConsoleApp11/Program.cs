ba0c2d47b4e3b00d4ccb80d27a00b1b7cf923f01
balandin228 <i.balandin@wellsoft.pro>
2021-01-22 00:48:25 +0500

    initial

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var processFileName  = "C:\\Users\\79090\\Desktop\\Processes\\netcoreapp3.1\\ConsoleApp10.exe";
+            using (var process = new Process(){})
+            {
+                process.StartInfo.FileName = processFileName;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.Start();
+                var k = process.Threads;
+                foreach (ProcessThread processThread in k)
+                {
+                    Console.WriteLine(processThread.Id);
+                }
+
+                process.Close();
+            }
+            var counter = new PerformanceCounter("Process", "% Processor Time", "ConsoleApp10");
+            var counter2 = new PerformanceCounter("Process", "% Processor Time", Process.GetCurrentProcess().ProcessName);
+            var counter3 = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+            while (true)
+            {
+                Console.Write("\r");
+                Console.Write("                                                                               ");
+                Console.Write("\r");
+                Console.Write($"ConsoleApp10= {counter.NextValue()/ Environment.ProcessorCount}, ConsoleApp11={counter2.NextValue()/ Environment.ProcessorCount}, Total={counter3.NextValue()}");
+
+                Thread.Sleep(200);
+            }
+        }
+    }
+}
