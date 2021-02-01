53007b3d582597051c20e9857013a856bd8ee3db
balandin228 <i.balandin@wellsoft.pro>
2021-02-01 17:34:10 +0500

    temp

--- ConsoleApp10/Program.cs
+++ ConsoleApp10/Program.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿using Library;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.VisualBasic;
 
 public class App
 {
+    private static Mutex mut = new Mutex();
+
     private static IEnumerable<int> Fibonacci
     {
         get
@@ -26,11 +30,59 @@ public class App
     }
 	public static void Main()
     {
+        var suspensionToken = new SuspensionToken();
         var fileName = Process.GetCurrentProcess().ProcessName + DateTime.Now.GetHashCode();
-        Task.Run(() => DoProcess(fileName)).Wait();
+
+        var checkThread = new Thread(() => CheckCpuUsage(suspensionToken));
+        checkThread.Start();
+        for (var i = 0; i < 5; i++)
+        {
+            var workThread = new Thread(() => DoProcess(fileName+i, suspensionToken));
+            workThread.Name = i + fileName;
+            workThread.Start();
+        }
+        //mut.WaitOne();
+        //while (true)
+        //{
+        //    if (suspensionToken.Suspended && suspensionToken.WorkThreadTaken)
+        //    {
+        //        mut.WaitOne();
+        //        suspensionToken.WorkThreadTaken = false;
+        //    }
+
+        //    if (!suspensionToken.Suspended && suspensionToken.WorkThreadTaken)
+        //    {
+        //        mut.ReleaseMutex();
+
+        //    }
+        //}
     }
 
-    public static async Task DoProcess(string fileName)
+    public  static void CheckCpuUsage(SuspensionToken token)
+    {
+        var currentProcessCounter = new PerformanceCounter("Process", "% Processor Time", Process.GetCurrentProcess().ProcessName);
+        var totalCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+        var taken = true;
+        while (true)
+        {
+            var processCpu = currentProcessCounter.NextValue() / Environment.ProcessorCount;
+            Console.Write("\r");
+            Console.Write("                                                                               ");
+            Console.Write("\r");
+            Console.Write($"ConsoleApp10={processCpu}, Total={totalCounter.NextValue()}");
+
+            Thread.Sleep(200);
+            //if (cpu > 10)
+            //{
+            //    token.Suspend();
+            //}
+            //else
+            //{
+            //    token.Release();
+            //}
+        }
+    }
+    public static async Task DoProcessAsync(string fileName, SuspensionToken token)
     {
         var pathToDirectory = Directory.GetCurrentDirectory();
         var fullPath = $"{pathToDirectory}\\{fileName}.txt";
@@ -43,9 +95,46 @@ public class App
         await using var sw = new StreamWriter(fullPath);
         foreach (var i in Fibonacci)
         {
-            await sw.WriteAsync(i.ToString());
+            if(token.Suspended)
+                mut.ReleaseMutex();
+            if (mut.WaitOne())
+            {
+                await sw.WriteAsync(i.ToString());
 
-            Console.WriteLine(i);
+                //Console.WriteLine(i);
+            }
+            else
+            {
+                Console.WriteLine("not get Mutex");
+            }
+        }
+    }
+
+    public static void DoProcess(string fileName, SuspensionToken token)
+    {
+        var pathToDirectory = Directory.GetCurrentDirectory();
+        //var fullPath = $"{pathToDirectory}\\{fileName}.txt";
+        //if (!File.Exists(fullPath))
+        //{
+        //    var fs = File.Create(fullPath);
+        //    fs.Close();
+        //}
+
+        //using var sw = new StreamWriter(fullPath);
+        foreach (var i in Fibonacci)
+        {
+            //mut.WaitOne();
+            //if (token.Suspended)
+            //{
+            //    mut.ReleaseMutex();
+            //    token.WorkThreadTaken = true;
+            //    Thread.Sleep(1000);
+            //    Console.WriteLine("mutex was released by workThread");
+            //}
+            //else
+            //sw.Write(i.ToString());
+
+                //Console.WriteLine(i);
         }
     }
 }
---
+++ Library/SuspensionToken.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace Library
+{
+    public class SuspensionToken
+    {
+
+        public bool Suspended { get; private set; } = true;
+        public bool WorkThreadTaken { get; set; } = true;
+        public SuspensionToken()
+        {
+        }
+
+
+        public void Suspend()
+        {
+            Suspended = true;
+        }
+
+        public void Release()
+        {
+            Suspended = false;
+        }
+    }
+}
